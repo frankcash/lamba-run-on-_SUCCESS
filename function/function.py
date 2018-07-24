e4a3ba318bb710c6fa188a982f6b21db73a3e12e
Frank Cash <cashc@acm.org>
2018-07-24 15:43:20 -0400

    Updates main.tf

---
+++ function/function.py
@@ -0,0 +1,17 @@
+import boto3
+import datetime
+
+exporters = {}
+
+def handler(event, context):
+    s3 = boto3.client('s3')
+    keys = []
+    objs = s3.list_objects(Bucket='lambda-run-on-success', Prefix="status/")
+    for obj in objs["Contents"]:
+        key = obj["Key"]
+        
+        status = key.split('/')[-1]
+        keys.append(status)
+
+    if '_SUCCESS' in keys:
+        print("FOUND")
